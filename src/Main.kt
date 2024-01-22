1298e8b9a61e3a843780b937c54b6622f5beeb4f
Almaz Nurmanov <almazbrr@gmail.com>
2024-01-22 16:46:28 -0500

    done!

--- src/Main.kt
+++ src/Main.kt
@@ -2,15 +2,25 @@ class ListNode(var `val`: Int){
 	var next: ListNode?=null
 }
 fun hasCycle(head: ListNode?): Boolean {
-	var curr = head
-	var counter=0
-	while (curr?.next!=null){
-		curr=curr.next
-		counter++
-		if (counter>10000)return true
+	fun hasCycle(head: ListNode?): Boolean {
+		var curr = head
+		var counter=0
+		var sum=0
+		var sum_max=-1000000
+		var sum_qty=0
+		while (curr?.next!=null){
+			sum+=curr.`val`
+			curr=curr.next
+			if (sum_max==sum)sum_qty++
+			if (sum_max<sum)sum_max=sum
+
+			if (sum_qty>2) return true
+			counter++
+
+			if (counter>10000)return true
+		}
+		return false
 	}
-	return false
-}
 fun main() {
 	println("Hello World!")
 
