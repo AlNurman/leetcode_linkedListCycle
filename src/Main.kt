4892dfa0f48a37808357245090827045e3a503c1
Almaz Nurmanov <almazbrr@gmail.com>
2024-01-22 16:26:33 -0500

    done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
+class ListNode(var `val`: Int){
+	var next: ListNode?=null
+}
+fun hasCycle(head: ListNode?): Boolean {
+	var curr = head
+	var counter=0
+	while (curr?.next!=null){
+		curr=curr.next
+		counter++
+		if (counter>10000)return true
+	}
+	return false
+}
 fun main() {
-    println("Hello World!")
+	println("Hello World!")
+
 }
